b6cc3129d5e7cf59378de8ee935de8690078a75f
Pascal Albisser <pascal@Pascals-MacBook-Pro.local>
2024-04-18 16:51:17 +0200

    DAG to test S3-Connection

---
+++ dags/s3_upload.py
@@ -0,0 +1,46 @@
+import json
+import os
+from datetime import datetime
+
+from airflow.models import DAG, Variable
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+
+def upload_to_s3(key_prefix: str) -> None:
+    hook = S3Hook('S3_Conn')
+
+    # Your Python dictionary
+    data = {
+        "key1": "value1",
+        "key2": "value2",
+        "key3": "value3"
+    }
+
+    # Convert dictionary to JSON
+    json_data = json.dumps(data)
+
+    # Retrieve S3 bucket name from Airflow variables
+    s3_bucket_name = Variable.get("S3_BUCKET_NAME")
+
+    key = os.path.join(key_prefix, 'my_new_example.json')
+
+    print(f'upload to {s3_bucket_name}/{key}')
+
+    hook.load_string(json_data, key=key, bucket_name=s3_bucket_name)
+
+
+with DAG(
+        dag_id='s3_dag',
+        schedule_interval='@daily',
+        start_date=datetime(2024, 4, 12),
+        catchup=False
+) as dag:
+    # Upload the file
+    task_upload_to_s3 = PythonOperator(
+        task_id='upload_to_s3',
+        python_callable=upload_to_s3,
+        op_kwargs={
+            'key_prefix': 'example',
+        }
+    )
