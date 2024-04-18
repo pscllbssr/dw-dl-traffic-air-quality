3edbbfce2479ea5f6a29690cf6cc278043c3399b
Pascal Albisser <pascal@Pascals-MacBook-Pro.local>
2024-04-19 00:00:30 +0200

    add all routes, instead of just one

--- dags/ingest_gmaps_route.py
+++ dags/ingest_gmaps_route.py
@@ -22,8 +22,21 @@ API_KEY = Variable.get("GMAPS_API_KEY")
 ROUTES_API_URL = "https://routes.googleapis.com/directions/v2:computeRoutes"
 
 
-def get_route(start, end, via):
+def get_route(start, end, via_lat, via_lon):
     url = f'{ROUTES_API_URL}?key={API_KEY}'
+
+    start = {
+        "address": start
+    }
+    end = {
+        "address": end
+    }
+    via = [{"location": {
+        "latLng": {
+            "latitude": via_lat,
+            "longitude": via_lon
+        }}, "via": True
+    }]
 
     response = requests.post(url, json={
         'origin': start,
@@ -50,69 +63,73 @@ def get_route(start, end, via):
         return None
 
 
-def ingest_route(start: str, stop: str, via_lat: float, via_lon: float):
-    start = {
-        "address": start
-    }
-    end = {
-        "address": stop
-    }
-    via = [{"location": {
-        "latLng": {
-            "latitude": via_lat,
-            "longitude": via_lon
-        }}, "via": True
-    }]
-
-    route_response = get_route(start, end, via)
-
-    return route_response
-
-
-def save_to_file(**kwargs) -> None:
-    data = kwargs['ti'].xcom_pull(task_ids='ingest_route')
-
+# Function to save route data to S3
+def save_to_s3(data, key_prefix, station):
     hook = S3Hook('S3_Conn')
-
-    print(f"saving data {data}")
-
-    # Convert dictionary to JSON
     json_data = json.dumps(data)
-
-    # Retrieve S3 bucket name from Airflow variables
     s3_bucket_name = Variable.get("S3_BUCKET_NAME")
-
     current_timestamp = datetime.now()
-
-    key = os.path.join(kwargs['key_prefix'], kwargs['station'], f'{current_timestamp.isoformat()}.json')
-
-    print(f"write to {s3_bucket_name}/{key}")
-
+    key = os.path.join(key_prefix, station, f'{current_timestamp.isoformat()}.json')
     hook.load_string(json_data, key=key, bucket_name=s3_bucket_name)
 
-    return {}
 
+# Define routes
+routes = [{
+   "start": "Stampfenbachstrasse 52, 8006 Zürich",
+   "stop": "Schaffhauserstrasse 40, 8006 Zürich",
+   "via_lat": 47.3868,
+   "via_lon": 8.5398,
+   "station_id": "Zch_Stampfenbachstrasse"
+}, {
+   "start": "Splügenstrasse 14, 8002 Zürich",
+   "stop": "Seebahnstrasse 110, 8003 ",
+   "via_lat": 47.371,
+   "via_lon": 8.5235,
+   "station_id": "Zch_Schimmelstrasse"
+}, {
+   "start": "Heinrichstrasse 269, 8005 Zürich",
+   "stop": "Bucheggstrasse 64, 8057 Zürich",
+   "via_lat": 47.3952,
+   "via_lon": 8.5261,
+   "station_id": "Zch_Rosengartenstrasse"
+}, {
+   "start": "Susenbergstrasse 11, 8044 Zürich",
+   "stop": "Susenbergstrasse 118, 8044 Zürich",
+   "via_lat": 47.3815,
+   "via_lon": 8.5659,
+   "station_id": "Zch_Heubeeribüel"
+}, {
+   "start": "Nordstrasse 174, 8037 Zürich",
+   "stop": "Nordstrasse 354, 8037 Zürich",
+   "via_lat": 47.3943,
+   "via_lon": 8.5253,
+   "station_id": "Zch_Rosengartenbrücke"
+}]
 
-# Step 3: Creating DAG Object
-dag = DAG(dag_id='ingest_gmaps_route',
-          default_args=default_args,
-          schedule_interval='@hourly',
-          catchup=False
-          )
+# Define DAG
+with DAG(dag_id='ingest_gmaps_routes', default_args=default_args, schedule_interval='@hourly', catchup=False) as dag:
+    for i, route in enumerate(routes):
 
-# Step 4: Creating task
-# Creating first task
-ingest_route = PythonOperator(task_id='ingest_route', dag=dag, python_callable=ingest_route,
-                              op_kwargs={
-                                  'start': 'Stampfenbachstrasse 52, 8006 Zürich',
-                                  'stop': 'Schaffhauserstrasse 40, 8006 Zürich',
-                                  'via_lat': 47.3868,
-                                  'via_lon': 8.5398,
-                              })
+        # Fetching the route ETA
+        fetch_route_task_id = f'fetch_route_{i}_{route["station_id"]}'
+        fetch_route_task = PythonOperator(
+            task_id=fetch_route_task_id,
+            python_callable=get_route,
+            op_kwargs={'start': route['start'], 'end': route['stop'], 'via_lat': route['via_lat'],
+                       'via_lon': route['via_lon'],
+                       'key_prefix': 'gmaps_routes', 'station': route['station_id']},
+            provide_context=True
+        )
 
-save_route = PythonOperator(task_id='save_to_file', dag=dag, python_callable=save_to_file,
-                            op_kwargs={'key_prefix': 'gmaps_routes', 'station': "station-Zch_Rosengartenstrasse"},
-                            provide_context=True)
+        # Saving to S3 task
+        save_to_s3_task_id = f'save_to_s3_{i}_{route["station_id"]}'
+        save_to_s3_task = PythonOperator(
+            task_id=save_to_s3_task_id,
+            python_callable=save_to_s3,
+            op_kwargs={'data': "{{ task_instance.xcom_pull(task_ids='" + fetch_route_task_id + "') }}",
+                       'key_prefix': 'gmaps_routes', 'station': f"station-Zch_Rosengartenstrasse_{i}"},
+            provide_context=True
+        )
 
-# Step 5: Setting up dependencies
-ingest_route >> save_route
+        # Set up task dependencies
+        fetch_route_task >> save_to_s3_task
