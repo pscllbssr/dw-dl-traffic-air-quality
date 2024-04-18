bbf9ee7fa1c40705304c3616d14a901167f913d6
Pascal Albisser <pascal@Pascals-MacBook-Pro.local>
2024-04-18 11:32:21 +0200

    add google route dag, update description

--- README.md
+++ README.md
@@ -2,25 +2,56 @@
 
 Project in Data Lake & Data Warehouse module at HSLU, FS24.
 
-## Setup locally
+## Setup
 
-### install
-Make sure you have a virtual-environment ready. This will install airflow running with a SQLite-Database, which is not recommended for production.
+We mostly follow the [airflow-docker-setup](https://airflow.apache.org/docs/apache-airflow/stable/howto/docker-compose/index.html). Make sure you have docker installed.
+
+### initialize
+
+On Linux, we have to create the folders first
+
 ```shell
-# set airflow directory to the current directory
-export AIRFLOW_HOME=$(pwd)/airflow
+mkdir -p ./dags ./logs ./plugins ./config
+echo -e "AIRFLOW_UID=$(id -u)" > .env
+```
 
-# set dag home folder
-export AIRFLOW__CORE__DAGS_FOLDER=$(pwd)/dags
+To initialize the database run
 
-# install with script
-./install-airflow-locally.sh
+```shell
+docker compose up airflow-init
 ```
 
 ### run
 
-```shell
-airflow webserver --port 8080
+Once initialized, start things up. May take 1-2 minutes. 
 
-airflow scheduler
+```shell
+docker compose up
 ```
+
+Now you should be able to access the web-interface at <http://localhost:8080>
+
+### connect S3
+
+In order to use S3 we need to do the following:
+
+1) connect our bucket via web-interface (Admin --> Connections --> Add a new record).
+2) add the bucket name as variable  (Admin --> Variables --> Add a new record). Use the key `S3_BUCKET_NAME`.
+
+In code you may now use it as follows:
+```python
+s3_bucket_name = Variable.get("S3_BUCKET_NAME")
+```
+
+### other variables
+
+Make sure to add all other necessary variables:
+
+- `GMAPS_API_KEY`
+- ...
+
+## Develop
+
+Create new DAGs in the `/dags`-folder. As it is mounted as a volume to the docker-container, new dags should appear in the web-interface after some seconds. 
+
+TODO: run DAGS from the CLI with docker setup.
---
+++ dags/ingest_gmaps_route.py
@@ -0,0 +1,118 @@
+# Step 1: Importing Modules
+# To initiate the DAG Object
+# Importing datetime and timedelta modules for scheduling the DAGs
+import json
+import os
+from datetime import datetime
+
+import requests
+from airflow import DAG
+from airflow.models import Variable
+# Importing operators
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+# Step 2: Initiating the default_args
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 4, 18),
+}
+
+API_KEY = Variable.get("GMAPS_API_KEY")
+ROUTES_API_URL = "https://routes.googleapis.com/directions/v2:computeRoutes"
+
+
+def get_route(start, end, via):
+    url = f'{ROUTES_API_URL}?key={API_KEY}'
+
+    response = requests.post(url, json={
+        'origin': start,
+        'destination': end,
+        'intermediates': via,
+        "travelMode": "DRIVE",
+        "routingPreference": "TRAFFIC_AWARE_OPTIMAL",
+    }, headers={
+        # specify the fields to return
+        'X-Goog-FieldMask': 'routes.distanceMeters,routes.duration,routes.staticDuration'
+    })
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 403:
+        print("Error: Authentication failed, no valid API key. Unable to fetch directions.")
+        return None
+    elif response.status_code == 400:
+        print("Error: Malformed request. Unable to fetch directions.")
+        return None
+    else:
+        print("Error: Unable to fetch directions.")
+        return None
+
+
+def ingest_route(start: str, stop: str, via_lat: float, via_lon: float):
+    start = {
+        "address": start
+    }
+    end = {
+        "address": stop
+    }
+    via = [{"location": {
+        "latLng": {
+            "latitude": via_lat,
+            "longitude": via_lon
+        }}, "via": True
+    }]
+
+    route_response = get_route(start, end, via)
+
+    return route_response
+
+
+def save_to_file(**kwargs) -> None:
+    data = kwargs['ti'].xcom_pull(task_ids='ingest_route')
+
+    hook = S3Hook('S3_Dev')
+
+    print(kwargs)
+
+    print(f"saving data {data}")
+
+    # Convert dictionary to JSON
+    json_data = json.dumps(data)
+
+    # Retrieve S3 bucket name from Airflow variables
+    s3_bucket_name = Variable.get("S3_BUCKET_NAME")
+
+    current_timestamp = datetime.now()
+
+    key = os.path.join(kwargs['key_prefix'], kwargs['station'], f'{current_timestamp.isoformat()}.json')
+
+    hook.load_string(json_data, key=key, bucket_name=s3_bucket_name)
+
+    return {}
+
+
+# Step 3: Creating DAG Object
+dag = DAG(dag_id='ingest_gmaps_route',
+          default_args=default_args,
+          schedule_interval='@once',
+          catchup=False
+          )
+
+# Step 4: Creating task
+# Creating first task
+ingest_route = PythonOperator(task_id='ingest_route', dag=dag, python_callable=ingest_route,
+                              op_kwargs={
+                                  'start': 'Stampfenbachstrasse 52, 8006 Zürich',
+                                  'stop': 'Schaffhauserstrasse 40, 8006 Zürich',
+                                  'via_lat': 47.3868,
+                                  'via_lon': 8.5398,
+                              })
+
+save_route = PythonOperator(task_id='save_to_file', dag=dag, python_callable=save_to_file,
+                            op_kwargs={'key_prefix': 'gmaps_routes', 'station': "station-Zch_Rosengartenstrasse"},
+                            provide_context=True)
+
+# Step 5: Setting up dependencies
+ingest_route >> save_route
